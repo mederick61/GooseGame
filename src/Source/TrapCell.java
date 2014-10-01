8c7a3d54795075c720766b0c1043c51b8da77066
BriceN <brice.nuzzo@gmail.com>
2014-10-01 12:07:37 +0200

    Javadoc terminée sauf Board. README uploadé

--- src/Source/TrapCell.java
+++ src/Source/TrapCell.java
@@ -1,17 +1,47 @@
 package Source;
 
+/**
+ * <b>A TrapCell keeps the player until the next event.
+ * <p>
+ * A TrapCell has no particular field, excepted those of its mother (Cell).
+ * </p>
+ * 
+ * 
+ * @author Leo JUMEL et Brice NUZZO
+ * @version 1.0
+ */
 
 public class TrapCell extends Cell{
-
+	/**
+     * TrapCell constructor
+     * <p>
+     * A TrapCell is built with an index.
+     * </p>
+     * 
+     * @param idx
+     *            The TrapCell index
+     */
 	public TrapCell(int idx) {
 		super(idx);
 	}
-	
+	/**
+	 * Indicates if a player occupying this cell can, at this turn, leave the
+	 * cell.
+	 *
+	 * @return true if and only if the player in this cell can freely leaves the
+	 *         cell at this turn
+	 */	
 	@Override
 	public boolean canBeLeftNow() {
 		return false;
 	}
-	
+	/**
+	 * Indicates if a cell holds a player until another player reaches the same
+	 * cell.
+	 *
+	 * @return true if and only if the only way for a player to get out of this
+	 *         cell is for another player to replace him
+	 */	
 	@Override
 	public boolean isRetaining() {
 		return true;
--- src/Source/WaitCell.java
+++ src/Source/WaitCell.java
@@ -1,17 +1,47 @@
 package Source;
+/**
+ * <b>A WaitCell keeps the player for a number of turns.
+ * <p>
+ * A WaitCell is characterized by the following informations :
+ * <ul>
+ * <li>A waiting time which represents the number of turns to wait. </li>
+ * <li>A waiting counter which is decremented each turn.</li>
+ * </ul>
+ * </p>
+ * @author Leo JUMEL et Brice NUZZO
+ * @version 1.0
+ */
 
 
 public class WaitCell extends Cell {
 
 	private int WaitingTime;
 	private int WaitingCounter;
-	
+
+	/**
+	 * WaitCell constructor
+	 * <p>
+	 * A WaitCell is built with an index.
+	 * </p>
+	 * 
+	 * @param idx
+	 *            The WaitCell index
+	 * @param time
+	 *            The WaitCell time to wait.
+	 */
 	public WaitCell(int idx, int time) {
 		super(idx);
 		this.WaitingTime = time;
 		this.WaitingCounter = time;
 	}
 	
+	/**
+	 * Indicates if a cell holds a player until another player reaches the same
+	 * cell.
+	 *
+	 * @return true if and only if the only way for a player to get out of this
+	 *         cell is for another player to replace him
+	 */
 	@Override
 	public boolean canBeLeftNow() {
 		if (this.WaitingCounter == 0)
@@ -24,7 +54,12 @@ public class WaitCell extends Cell {
 			return false;
 		}
 	}
-	
+	/**
+	 * Remembers <code>player</code> to be in this cell.
+	 *
+	 * @param player
+	 *            the new player in this cell
+	 */	
 	@Override
 	public void welcome(Player player) {
 		this.WaitingCounter = this.WaitingTime;
